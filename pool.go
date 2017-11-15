9f47c19ad03bf4da21836282b721f75fad678a54
侯志良 <zhiliang.hou@dmall.com>
2017-11-15 14:54:01 +0800

    move pool logic to lovego/pool

--- pool.go
+++ pool.go
@@ -4,14 +4,12 @@ import (
 	"net/url"
 	"strconv"
 	"sync"
+
+	"github.com/lovego/pool"
 )
 
 type Pool struct {
-	maxOpen int // <= 0 means unlimited
-	maxIdle int // <  0 means unlimited
-	opener  func() (*Mailer, error)
-	mailers []*Mailer
-	mu      sync.Mutex
+	pool *pool.Pool
 }
 
 func NewPool(mailerUrl string) (*Pool, error) {
@@ -19,35 +17,18 @@ func NewPool(mailerUrl string) (*Pool, error) {
 	if err != nil {
 		return nil, err
 	}
-	maxOpen, maxIdle, err := poolParams(uri.Query())
-	if err != nil {
-		return nil, err
-	}
 	opener, err := getOpener(uri)
 	if err != nil {
 		return nil, err
 	}
+	p, err := pool.New(opener, uri.Query())
+	if err != nil {
+		return nil, err
+	}
 
-	return &Pool{
-		maxOpen: maxOpen, maxIdle: maxIdle, opener: opener,
-	}, nil
+	return &Pool{pool: p}, nil
 }
 
 func (p *Pool) Send() error {
 	return nil
-}
-
-func poolParams(q url.Values) (maxOpen, maxIdle int, err error) {
-	maxOpen, maxIdle = 10, 3
-	if str := q.Get(`maxOpen`); str != `` {
-		if maxOpen, err = strconv.Atoi(str); err != nil {
-			return
-		}
-	}
-	if str := q.Get(`maxIdle`); str != `` {
-		if maxIdle, err = strconv.Atoi(str); err != nil {
-			return
-		}
-	}
-	return
 }
